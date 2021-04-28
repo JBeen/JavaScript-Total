e761a529d2c87e18796d822a618d07b85de1014f
JBeen <alexandr.kravchuk@gmail.com>
2021-04-28 19:50:51 +0200

    Fixed animation deletion

--- public/js/questions/create-game.js
+++ public/js/questions/create-game.js
@@ -1,10 +1,9 @@
-import questions from './questions.js'
-import { hljs } from '../helpers/hl.js'
-import { generatePage } from '../helpers/generate-page.js'
-import { animateDetails } from '../helpers/animate-details.js'
+import questions from "./questions.js";
+import { hljs } from "../helpers/hl.js";
+import { generatePage } from "../helpers/generate-page.js";
 
 export const createGame = () => {
-  top_btn.style.display = 'none'
+  top_btn.style.display = "none";
 
   const template = `
   <section id="game_box">
@@ -14,58 +13,58 @@ export const createGame = () => {
     <output id="progress"></output>
     <div id="question_box"></div>
   </section>
-  `
+  `;
 
-  main.innerHTML = template
+  main.innerHTML = template;
 
   close_btn.addEventListener(
-    'click',
+    "click",
     (ev) => {
-      ev.target.parentElement.remove()
-      generatePage()
+      ev.target.parentElement.remove();
+      generatePage();
     },
     {
       once: true
     }
-  )
+  );
 
-  const set = new Set()
+  const set = new Set();
 
   while (set.size < 20) {
-    const i = Math.floor(Math.random() * (questions.length + 1))
+    const i = Math.floor(Math.random() * (questions.length + 1));
 
     if (i > questions.length || i < 0) {
-      i = 0
+      i = 0;
     }
 
-    set.add(questions[i])
+    set.add(questions[i]);
   }
 
-  const array = [...set]
+  const array = [...set];
 
-  let rightAnswers = 0
-  let wrongAnswers = 0
-  let i = 0
+  let rightAnswers = 0;
+  let wrongAnswers = 0;
+  let i = 0;
 
-  updateScoreAndProgress()
+  updateScoreAndProgress();
 
   function updateScoreAndProgress() {
-    score.innerHTML = `<span class="right">${rightAnswers}</span> - <span class="wrong">${wrongAnswers}</span>`
+    score.innerHTML = `<span class="right">${rightAnswers}</span> - <span class="wrong">${wrongAnswers}</span>`;
 
-    progress.textContent = `${i + 1} / ${array.length}`
+    progress.textContent = `${i + 1} / ${array.length}`;
   }
 
-  createQuestion()
+  createQuestion();
 
   function createQuestion() {
     if (i === array.length) {
-      showResult()
-      return
+      showResult();
+      return;
     }
 
-    const { question, rightAnswer, explanation } = array[i]
+    const { question, rightAnswer, explanation } = array[i];
 
-    const answers = array[i].answers.trim().split('\n')
+    const answers = array[i].answers.trim().split("\n");
 
     const template = `
     <pre>
@@ -76,7 +75,7 @@ export const createGame = () => {
     <ul>
       ${answers.reduce(
         (html, i) => (html += `<li class="answer_item">${i}</li>`),
-        ''
+        ""
       )}
     </ul>
     <button id="answer_btn">Ответить</button>
@@ -87,41 +86,39 @@ export const createGame = () => {
         <p>${explanation}</p>
       </div>
     </details>
-    `
+    `;
 
-    question_box.innerHTML = template
+    question_box.innerHTML = template;
 
-    hljs(globalThis)
+    hljs(globalThis);
 
-    animateDetails()
+    question_box.querySelector("li").classList.add("checked");
 
-    question_box.querySelector('li').classList.add('checked')
-
-    question_box.addEventListener('click', (ev) => {
-      if (ev.target.className === 'answer_item') {
-        if (ev.target.classList.contains('checked')) {
-          return
+    question_box.addEventListener("click", (ev) => {
+      if (ev.target.className === "answer_item") {
+        if (ev.target.classList.contains("checked")) {
+          return;
         }
 
-        question_box.querySelector('.checked').classList.remove('checked')
+        question_box.querySelector(".checked").classList.remove("checked");
 
-        ev.target.classList.add('checked')
+        ev.target.classList.add("checked");
       }
-    })
+    });
 
     answer_btn.addEventListener(
-      'click',
+      "click",
       () => {
-        const answers = [...question_box.querySelectorAll('li')]
+        const answers = [...question_box.querySelectorAll("li")];
 
         const userAnswerEl = answers.find((a) =>
-          a.classList.contains('checked')
-        )
+          a.classList.contains("checked")
+        );
         const rightAnswerEl = answers.find(
           (a) => a.textContent[0] === rightAnswer
-        )
+        );
 
-        const userAnswer = userAnswerEl.textContent.substr(0, 1)
+        const userAnswer = userAnswerEl.textContent.substr(0, 1);
 
         checkAnswer(
           userAnswerEl,
@@ -129,12 +126,12 @@ export const createGame = () => {
           userAnswer,
           rightAnswer,
           answer_btn
-        )
+        );
       },
       {
         once: true
       }
-    )
+    );
   }
 
   function checkAnswer(
@@ -144,55 +141,55 @@ export const createGame = () => {
     rightAnswer,
     button
   ) {
-    let isRight = true
+    let isRight = true;
 
     if (userAnswer === rightAnswer) {
-      rightAnswers++
+      rightAnswers++;
     } else {
-      wrongAnswers++
-      isRight = false
+      wrongAnswers++;
+      isRight = false;
     }
 
     if (isRight) {
-      userAnswerEl.classList.add('right')
+      userAnswerEl.classList.add("right");
     } else {
-      userAnswerEl.classList.add('wrong')
-      rightAnswerEl.click()
-      rightAnswerEl.classList.add('right')
-      question_box.querySelector('summary').click()
+      userAnswerEl.classList.add("wrong");
+      rightAnswerEl.click();
+      rightAnswerEl.classList.add("right");
+      question_box.querySelector("summary").click();
     }
 
-    button.textContent = 'Дальше'
+    button.textContent = "Дальше";
 
     button.addEventListener(
-      'click',
+      "click",
       () => {
-        i++
-        updateScoreAndProgress()
-        createQuestion()
+        i++;
+        updateScoreAndProgress();
+        createQuestion();
       },
       {
         once: true
       }
-    )
+    );
   }
 
   function showResult() {
-    const percentage = ((rightAnswers / array.length) * 100).toFixed()
+    const percentage = ((rightAnswers / array.length) * 100).toFixed();
 
-    let result
+    let result;
 
     if (percentage >= 80) {
-      result = `Отличный результат! Вы прекрасно знаете JavaScript.`
+      result = `Отличный результат! Вы прекрасно знаете JavaScript.`;
     } else if (percentage > 50) {
-      result = `Неплохой результат, но есть к чему стремиться. Дерзайте!`
+      result = `Неплохой результат, но есть к чему стремиться. Дерзайте!`;
     } else {
-      result = `Похоже, вы только начали изучать JavaScript. Удачи!`
+      result = `Похоже, вы только начали изучать JavaScript. Удачи!`;
     }
 
-    main.querySelector('h3').remove()
-    score.remove()
-    progress.remove()
+    main.querySelector("h3").remove();
+    score.remove();
+    progress.remove();
 
     question_box.innerHTML = `
         <h3>Ваш результат</h3>
@@ -200,6 +197,6 @@ export const createGame = () => {
         <p>Неправильных ответов: <span class="wrong">${wrongAnswers}</span></p>
         <p>Процент правильных ответов: ${percentage}</p>
         <p>${result}</p>
-        `
+        `;
   }
-}
+};
